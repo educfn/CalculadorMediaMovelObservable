05a665eaf710b45f0ef7b920f7df445673b3af4b
Eduardo CFN <eduardo@verion.ind.br>
2021-07-13 18:19:48 -0300

    *(Devolpment_Branch)Corrigido nome do namespace incorreto na InitialPage.xaml.cs.

--- CalculadorMediaMovel/CalculadorMediaMovel/Screens/InitialPage.xaml.cs
+++ CalculadorMediaMovel/CalculadorMediaMovel/Screens/InitialPage.xaml.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace CalculadorMediaMovel.Screens
+namespace CalculadorMediaMovelObservable.Screens
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class InitialPage : ContentPage
